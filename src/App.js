5c2c9b1bdfeccf9971a0160534c6906bc0d62018
zidan4 <zidanjaffar@gmail.com>
2026-09-05 22:47:07 +0300

    component working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet'
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Dev Env
         </a>
+        <Greet />
       </header>
     </div>
   );
